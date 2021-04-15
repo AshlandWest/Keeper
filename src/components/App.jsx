8e056383d8ad3704b6034d6fb3df1f0fb67b713a
Ash <ashlandmwest@gmail.com>
2021-04-15 14:07:58 -0700

    Add test function and fix delete issue

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -19,13 +19,64 @@ function App() {
     });
   }
 
+  function deleteNote(index) {
+    const newNotes = [];
+    setNotes((prevNotes) => {
+      newNotes.push(...prevNotes);
+      newNotes.splice(index, 1);
+      return newNotes;
+    });
+  }
+
+  //testing only
+  function addTests() {
+    const newNotes = [];
+    setNotes((prevNotes) => {
+      newNotes.push(...prevNotes);
+      newNotes.push(
+        {
+          title: "Test title 1",
+          content: "Test content 1",
+        },
+        {
+          title: "Test title 2",
+          content: "Test content 2",
+        },
+        {
+          title: "Test title 3",
+          content: "Test content 3",
+        },
+        {
+          title: "Test title 4",
+          content: "Test content 4",
+        },
+        {
+          title: "Test title 5",
+          content: "Test content 5",
+        }
+      );
+      return newNotes;
+    });
+  }
+
   return (
     <div>
       <Header />
       <CreateArea createNote={createNote} />
       {notes.map((note, index) => {
-        return <Note key={index} title={note.title} content={note.content} />;
+        return (
+          <Note
+            key={index}
+            index={index}
+            title={note.title}
+            content={note.content}
+            delete={deleteNote}
+          />
+        );
       })}
+      <div>
+        <button onClick={addTests}>Add Tests</button>
+      </div>
       <Footer />
     </div>
   );
